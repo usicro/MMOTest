b4fc186ed04fe8a38f4e38f726b45f3c0efd509c
Lukas <Lukas@Lukas-PC.fritz.box>
2012-01-24 20:18:55 +0100

    DataParser FirstStable

--- src/mmotest/DataParser.java
+++ src/mmotest/DataParser.java
@@ -1,17 +1,36 @@
 package mmotest;
 
+import java.net.*;
+
 public class DataParser {
 
 	private static DataParser instance = null;
 	 
     private DataParser() {}
- 
+    
+    DatagramPacket packet;
 
     public static DataParser getInstance() {
         if (instance == null) {
             instance = new DataParser();
         }
-        return instance;
+        return instance;        
+    }
+    
+    public void parse(DatagramPacket incpacket) {
+    	byte[] buf = new byte[256];
+    	int id = (int) buf[0];
+    	
+    	switch(id) {
+    		case 1:
+    			login(incpacket); break;
+    		case 2:
+    			movement(incpacket); break;
+    		case 3:
+    			action(incpacket); break;
+    		default:
+    			System.out.println("Unknown ID: " + id);
+    	}
     }
 	
 }
